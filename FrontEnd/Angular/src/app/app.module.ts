696160e9d4bc00d0907ccb47823e9616560e8192
dnsing <dnsing17@gmail.com>
2020-10-27 21:28:23 -0600

    Primeras Vistas

--- FrontEnd/Angular/src/app/app.module.ts
+++ FrontEnd/Angular/src/app/app.module.ts
@@ -2,23 +2,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ConnectionServiceModule} from 'ng-connection-service';
+
 
 import { AppComponent } from './app.component';
-
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LobbyComponent } from './lobby/lobby.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    LobbyComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    ConnectionServiceModule,
+    
+
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+      { path: 'signup', component: SignupComponent},
+      { path: 'lobby', component: LobbyComponent},
+    
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
