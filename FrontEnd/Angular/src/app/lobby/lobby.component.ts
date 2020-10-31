fc0040cf2f3103038960bbea667e6a75bc48ee9e
garza3333 <garza3390@estudiantec.cr>
2020-10-31 17:13:40 -0600

    Creando Barra de Busqueda

--- FrontEnd/Angular/src/app/lobby/lobby.component.ts
+++ FrontEnd/Angular/src/app/lobby/lobby.component.ts
@@ -10,16 +10,60 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class LobbyComponent implements OnInit {
   validatingForm: FormGroup;
+  
+
 
   constructor() { }
 
+  
+
+  
+
   ngOnInit(): void {
+
     this.validatingForm = new FormGroup({
       signupFormModalName: new FormControl('', Validators.required),
       signupFormModalEmail: new FormControl('', Validators.email),
       signupFormModalPassword: new FormControl('', Validators.required),
     });
+    
+      
+    this.function();
+
   }
+
+
+ 
+  public function(){
+    
+    const countries = [
+      {name: 'USA'},
+      {name: 'India'},
+      {name: 'Argentina'},
+      {name: 'Armenia'}
+    ];
+    
+    const searchInput = document.querySelector('.search-input');
+    const suggestionsPanel = document.querySelector('.suggestions');
+    
+    searchInput.addEventListener('keyup', function() {
+    
+      const input = searchInput.value;
+      suggestionsPanel.innerHTML = '';
+      const suggestions = countries.filter(function(country) {
+        return country.name.toLowerCase().startsWith(input);
+      });
+      suggestions.forEach(function(suggested) {
+        const div = document.createElement('div');
+        div.innerHTML = suggested.name;
+        suggestionsPanel.appendChild(div);
+      });
+      if (input === '') {
+        suggestionsPanel.innerHTML = '';  
+      }
+    })
+  }
+
 
 
   get signupFormModalName() {
@@ -33,5 +77,9 @@ export class LobbyComponent implements OnInit {
   get signupFormModalPassword() {
     return this.validatingForm.get('signupFormModalPassword');
   }
+
+  
+
 }
+
 
